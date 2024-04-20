1ca33e86927581489ae5050d939301e327ac7cb8
SattyaP <stylearning7@gmail.com>
2024-04-20 22:10:59 +0700

    feat: Add compression, rate limiting, and security headers to server.js

--- src/server.js
+++ src/server.js
@@ -2,13 +2,30 @@ import express from "express";
 import cors from "cors";
 import "./loadenv.js";
 import products from "./routes/index.js";
+import compression from "compression";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 const PORT = process.env.PORT || 5050;
 const version = process.env.VERSION || "v1";
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, 
+  max: 20,
+});
+
 app.use(cors());
 app.use(express.json());
+app.use(compression());
+app.use(limiter);
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
 
 app.use(`/api/${version}/products`, products);
 
