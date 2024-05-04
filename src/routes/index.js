432d99aac202938e12308ebf26884f09b367df03
SattyaP <stylearning7@gmail.com>
2024-05-04 12:22:06 +0700

    fixing: bug method

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,10 @@ import ProductController from "../controllers/productController.js";
 const product = new ProductController();
 const router = express.Router();
 
-router.get('/', (_req, res) => product.index(_req, res));
-router.post('/', (req, res) => product.store(req, res));
-router.get('/:slug', (req, res) => product.show(req, res));
-router.put('/:slug', (req, res) => product.update(req, res));
-router.delete('/:slug', (req, res) => product.destroy(req, res));
+router.get('/', async (req, res) => await product.index(req, res));
+router.post('/', async (req, res) => await product.store(req, res));
+router.get('/:slug', async (req, res) => await product.show(req, res));
+router.put('/:slug', async (req, res) => await product.update(req, res));
+router.delete('/:slug', async (req, res) => await product.destroy(req, res));
 
 export default router;
